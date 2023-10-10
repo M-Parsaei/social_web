7da6e0d77ba6c9e924ca4a93d9038db9acaef540
GurkiratSinghArora <sgurkirat868@gmail.com>
2023-10-10 16:46:16 -0700

    Added basic express backend

---
+++ server/index.js
@@ -0,0 +1,17 @@
+
+const express  = require ("express");
+const mongoose = require ("mongoose");
+const port = 4000;
+
+const app = express();
+
+
+app.use(express.json());
+
+app.get("/", (req,res)=>{
+    res.send("Hello World");
+})
+
+app.listen(port, (req,res)=>{
+    console.log(`Server is running on port ${port}`);
+})
