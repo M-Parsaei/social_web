c3da4837240932e92122b7de1090206340897b2a
GurkiratSinghArora <sgurkirat868@gmail.com>
2023-10-13 18:09:38 -0700

    Added all the basic user routes get, delete, update, get followers, get following and add user following routes

--- server/controllers/UserControllers.js
+++ server/controllers/UserControllers.js
@@ -45,6 +45,59 @@ module.exports.updateUserContoller = async(req,res)=>{
         }catch(err){
             res.status(500).json(err);
         }
-          
+    }
+}
+
+module.exports.getUserFollowerController = async(req,res)=>{
+    try{
+        const user = await User.findOne({_id:req.params.id});
+        const followers = user.followers;
+        res.status(200).json({followers})
+    }
+    catch(error){
+        res.status(500).json({"Error":error});
+    }
+}
+
+module.exports.getUserFollowingController = async(req,res) =>{
+    try{
+        const user = await User.findOne({_id:req.params.id});
+        const followings = user.followings;
+        res.status(200).json({followings})
+    }
+    catch(error){
+        res.status(500).json({"Error":error});
+    }
+}
+
+module.exports.addUserFollowingController = async(req,res)=>{
+    try{
+        if(req.body.id === req.params.id){
+            return res.status(403).json("You can't follow yourself");
+        }
+        const user = await User.findById({_id:req.body.id});
+        const currentUser = await User.findById({_id:req.params.id});
+        if(user && currentUser){
+            if(!user.followings.includes(req.params.id)){
+                let userFollowing = user.followings;
+                if(userFollowing.length == 0){
+                    userFollowing = [req.params.id];
+                }
+                else{
+                    userFollowing = [
+                        ...user.following,req.params.id]
+                }
+                await User.findOneAndUpdate({_id:req.body.id}, {followings:userFollowing}); 
+                const userFollowers = [
+                    ...currentUser.followers,req.body.id];
+                    await User.findOneAndUpdate({_id:req.params.id},{followers:userFollowers});
+            }     
+        }
+        res.status(200).json("User has been followed");
+    }
+
+    catch(error){
+        console.log(error);
+        res.status(500).json({"Error":error});
     }
 }
--- server/routes/UserRouters.js
+++ server/routes/UserRouters.js
@@ -1,13 +1,14 @@
-const { getUserContoller, deleteUserContoller, updateUserContoller } = require("../controllers/UserControllers");
+const { getUserContoller, deleteUserContoller, updateUserContoller,getUserFollowerController, getUserFollowingController,addUserFollowingController } = require("../controllers/UserControllers");
 const User = require("../model/user");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 const router = require("express").Router();
 
 router.get("/:id", getUserContoller);
-
 router.delete("/:id", deleteUserContoller);
-
 router.put("/:id", updateUserContoller);
+router.get("/followers/:id", getUserFollowerController);
+router.post("/following/:id", getUserFollowingController);
+router.post("/addFollow/:id",addUserFollowingController);
 
 module.exports = router
 
