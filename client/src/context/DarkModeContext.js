0c661a322b6329b1007de422baca0b5409185bcf
M-Parsaei <khodadostetdarm@gmail.com>
2023-11-04 21:29:10 -0700

    chore: completed the dark mod functionality

--- client/src/context/DarkModeContext.js
+++ client/src/context/DarkModeContext.js
@@ -1,5 +1,9 @@
 import { createContext, useEffect, useReducer } from "react";
 
+
+export const DarkModeContext = createContext();
+
+
 const DarkModeReducer = (state,action)=>{
     switch(action.type){
         case "toggle":
@@ -9,10 +13,15 @@ const DarkModeReducer = (state,action)=>{
             return state;
     }
 }
-export const DarkModeContext = createContext();
 export const DarkModeContextProvider = (props) =>{
 
     const [state,dispatch]=useReducer(DarkModeReducer,true);
+
+    useEffect(()=>{
+        console.log("value of dark mode is ")
+        console.log(state);
+    },[state])
+
     return(
         <DarkModeContext.Provider value={{state,dispatch}}>
             {props.children}
